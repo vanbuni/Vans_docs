684a1ebc59cbd6c2634747f71b3e17f7a5597863
default-admin <default-admin@Van-Bradleys-MacBook-Pro.local>
2021-12-03 21:30:51 -0800

    Python youtube

---
+++ Python/functions/functions.py
@@ -0,0 +1,26 @@
+# def greet ():
+#     print("Hi there")
+#     print("Hi there 2")
+
+
+# greet()
+
+
+# def greet(first, last):
+#     print(f"Hi {first} {last}")
+#     print("Welcome aboard")
+
+# greet("Van", "Bradley")
+
+
+
+#Returns a value
+def get_greeting(name):
+    return f"Hi {name}"
+
+message = get_greeting("Mosh")
+
+
+
+
+
